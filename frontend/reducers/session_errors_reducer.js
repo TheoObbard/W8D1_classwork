670888b095791d35141e4f3d448d60526cfe8a3a
your name here <theo@dali.dartmouth.edu>
2018-12-10 17:14:40 -0800

    Complete session reducer, entities reducer, users reducer

---
+++ frontend/reducers/session_errors_reducer.js
@@ -0,0 +1,18 @@
+import { RECEIVE_SESSION_ERRORS, RECEIVE_CURRENT_USER } from '../actions/session_actions';
+
+const sessionReducer = (state = {}, action) {
+  Object.freeze(state)
+  const newState = Object.assign({}, state)
+  switch (action.type) {
+      case RECEIVE_SESSION_ERRORS:
+          newState[errors] = action.errors 
+          return newState;
+      case RECEIVE_CURRENT_USER:
+          newState[errors] = null;
+          return newState;
+      default:
+          return state;
+  }
+}
+
+export default sessionReducer;
--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -3,12 +3,14 @@ import LOGOUT_CURRENT_USER from '../actions/session_actions';
 
 export const sessionReducer = (state = {}, action) {
     Object.freeze(state)
-    const newState = Object.assign({})
+    const newState = Object.assign({}, state)
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
-            
+            newState[id] = action.user.id
+            return newState
         case LOGOUT_CURRENT_USER:
-
+            newState[id] = null
+            return newState
         default:
             return state;
     }
